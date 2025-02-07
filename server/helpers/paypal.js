f27c5cf7614120044512c299166db85723a84a2b
Maragada Srinu <srinumaragada@gmail.com>
2025-02-07 16:21:27 +0530

    Force sandbox mode for PayPal

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,7 +1,7 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: process.env.NODE_ENV === 'production' ? 'live' : 'sandbox',
+  mode: 'sandbox',
   client_id: process.env.PAYPAL_CLIENT_ID,
   client_secret: process.env.PAYPAL_CLIENT_SECRET,
 
