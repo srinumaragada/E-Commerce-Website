dfbd2e13c22f3a1cb03150896a0940143abbee5e
Maragada Srinu <srinumaragada@gmail.com>
2025-02-07 16:38:00 +0530

    update

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -71,7 +71,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, { 
       httpOnly: true, 
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-      sameSite: "none", // Required for cross-origin cookies
+      sameSite: "none", 
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     }).json({
       success: true,
